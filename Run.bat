44f845810c62a5a773d2fd91c4e13bdaeacea9af
Miltiadis Papaioannou <miltosianap@gmail.com>
2024-03-14 18:09:39 +0200

    First Commit

---
+++ Run.bat
@@ -0,0 +1,4 @@
+@npm run serve
+rem con = document.querySelectorAll('.column:first-child input')
+rem [].forEach.call(con, function(inp) {inp.click()});
+rem don't forger to uncomment on IpipMain
---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <Header />
+    <div class="container">
+			<Main />
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from "./components/layout/Header.vue";
+import Footer from "./components/layout/Footer.vue";
+import Main from "./components/forms/IpipMain.vue";
+
+export default {
+  name: "app",
+  components: {
+    Main
+  }
+};
+</script>
